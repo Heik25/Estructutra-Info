a63cdf2b2bf45233e519f333c53d078277e811c6
Reinel <Reinel@DESKTOP-VTH3I31>
2021-10-01 12:34:08 -0500

    Taller3

---
+++ src/main/java/co/edu/ucundinamarca/tallern/Libro.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.ucundinamarca.tallern;
+
+/**
+ *
+ * @author Reinel
+ */
+public class Libro {
+    private String titulo;
+    private String autor;
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+}
