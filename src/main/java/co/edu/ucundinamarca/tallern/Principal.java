d91c8512113bcca2f4e843643916fb68897554f2
Reinel <Reinel@DESKTOP-VTH3I31>
2021-10-27 12:52:34 -0500

    Taller7

---
+++ src/main/java/co/edu/ucundinamarca/tallern/Principal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.ucundinamarca.tallern;
+
+import co.edu.ucundinamarca.tallern.NodoCola.ColaLista;
+import co.edu.ucundinamarca.tallern.NodoPila.PilaLista;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Reinel
+ */
+public class Principal {
+
+    static Logger log = Logger.getLogger(Principal.class.getName());
+
+    public static void pila() {
+
+    }
+
+    public static void cola() {
+
+    }
+
+    public static void main(String[] args) {
+        pila();
+        cola();
+    }
+
+}
